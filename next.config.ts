1dd2df5277c9b84572750e2e01959c7c0e448099
Nguyen Xuan Trong <trongnx@gofiber.vn>
2026-02-12 16:29:16 +0700

    feat: implement custom fetch wrapper with typescript

--- next.config.ts
+++ next.config.ts
@@ -5,39 +5,7 @@ const nextConfig: NextConfig = {
   // reactCompiler: true,
   images: {
     formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'bo-api.medpro.com.vn',
-        port: '5000',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.medpro.vn',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'resource.medpro.com.vn',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cms.medpro.com.vn',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn-pkh.longvan.net',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'prod-partner.s3-hcm-r1.longvan.net',
-        pathname: '/**',
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
